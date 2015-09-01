aa10da9bcfcaa20c781b9e24534094323dcf6913
Keon Vafai <keylime503@gmail.com>
2015-08-31 23:18:30 -0700

    Added scraper to get TeamItem for all Pac12 teams

---
+++ cfbScraper/cfbScraper/spiders/team_spider.py
@@ -0,0 +1,22 @@
+import re
+import scrapy
+from cfbScraper.items import TeamItem
+
+class TeamSpider(scrapy.Spider):
+   name = "team"
+   allowed_domains = ["espn.com"]
+   start_urls = [ "http://espn.go.com/college-football/teams" ]
+   
+   def parse(self, response):
+      teamSelectorXPath = '//div[@class="mod-container mod-open-list '
+      teamSelectorXPath += 'mod-teams-list-medium mod-no-footer"]/'
+      teamSelectorXPath +=  'div[@class="mod-header colhead"]/'
+      teamSelectorXPath += 'h4[text()="Pac-12"]/../..//li'
+      for sel in response.xpath( teamSelectorXPath ):
+         teamItem = TeamItem()
+         teamItem[ 'name' ] = sel.xpath( 'h5/a/text()' ).extract()
+         espnIdRegex = '.*?([0-9]+)$'
+         teamItem[ 'espnId' ] = re.match( espnIdRegex,
+                                   sel.xpath( 'span/a/@href' ).extract()[0] ).group(1)
+         yield teamItem
+         
