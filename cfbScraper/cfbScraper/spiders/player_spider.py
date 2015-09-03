63361e78397da33b443dcb2fc7b19f53b55e232e
Keon Vafai <keylime503@gmail.com>
2015-09-03 01:08:40 -0700

    Player scraper now works and fills the db with all 12 statically-linked espn rosters

--- cfbScraper/cfbScraper/spiders/player_spider.py
+++ cfbScraper/cfbScraper/spiders/player_spider.py
@@ -6,7 +6,18 @@ class PlayerSpider(scrapy.Spider):
    name = "player"
    allowed_domains = ["espn.com"]
    # TODO: instead of hardcoding 12 team roster links, get it dynamically frpm team scraper's IDs
-   start_urls = [ "http://espn.go.com/college-football/team/roster/_/id/26/ucla-bruins" ]
+   start_urls = [ "http://espn.go.com/ncf/teams/roster?teamId=12",
+                  "http://espn.go.com/ncf/teams/roster?teamId=9",
+                  "http://espn.go.com/ncf/teams/roster?teamId=25",
+                  "http://espn.go.com/ncf/teams/roster?teamId=38",
+                  "http://espn.go.com/ncf/teams/roster?teamId=2483",
+                  "http://espn.go.com/ncf/teams/roster?teamId=204",
+                  "http://espn.go.com/ncf/teams/roster?teamId=24",
+                  "http://espn.go.com/ncf/teams/roster?teamId=26",
+                  "http://espn.go.com/ncf/teams/roster?teamId=30",
+                  "http://espn.go.com/ncf/teams/roster?teamId=254",
+                  "http://espn.go.com/ncf/teams/roster?teamId=264",
+                  "http://espn.go.com/ncf/teams/roster?teamId=265" ]
    
    def parse(self, response):
       playerSelectorXPath = '//table[@class="tablehead"]/tr'
@@ -16,10 +27,10 @@ class PlayerSpider(scrapy.Spider):
       for sel in response.xpath( playerSelectorXPath )[ 2 : ]:
          playerItem = PlayerItem()
          playerUrl = sel.xpath( './/td' )[ 1 ].xpath( './/a/@href' ).extract()[ 0 ]
-         playerItem[ 'playerId' ] = re.match( urlNumRegex, playerUrl ).group( 1 )
-         playerItem[ 'teamId' ] = teamId
          playerItem[ 'name' ] = sel.xpath( './/td/a/text()' ).extract()
          playerItem[ 'position' ] = sel.xpath( './/td/text()' ).extract()[ 1 ]
+         playerItem[ 'teamId' ] = teamId
+         playerItem[ 'espnId' ] = re.match( urlNumRegex, playerUrl ).group( 1 )
          if playerItem[ 'position' ] in [ 'QB', 'RB', 'WR' ]:
             yield playerItem
          
