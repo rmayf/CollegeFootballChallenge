505ae90c41012d75716f22c36b141672aaa875e5
Reid Mayfield <rmayf@arista.com>
2016-09-04 22:28:53 -0700

    calculateScores works on all previous weeks

--- django/Itthicron/calculateScores.py
+++ django/Itthicron/calculateScores.py
@@ -4,43 +4,41 @@ from leaderboard.models import Picks
 from django.core.exceptions import ObjectDoesNotExist
 from stats.models import *
 
-currentWeek = Itthicron.currentWeek()
-picks = Picks.objects.filter( week=currentWeek )
-for pick in picks:
-	
-	score = 0
-	try:
-		score += PlayerStat.objects.get( player=pick.QB1, week=currentWeek ).score
-	except ObjectDoesNotExist: 
-		pass
-	try:
-		score += PlayerStat.objects.get( player=pick.QB2, week=currentWeek ).score
-	except ObjectDoesNotExist: 
-		pass
-	try:
-		score += PlayerStat.objects.get( player=pick.RB1, week=currentWeek ).score
-	except ObjectDoesNotExist: 
-		pass
-	try:
-		score += PlayerStat.objects.get( player=pick.RB2, week=currentWeek ).score
-	except ObjectDoesNotExist: 
-		pass
-	try:
-		score += PlayerStat.objects.get( player=pick.WR1, week=currentWeek ).score
-	except ObjectDoesNotExist: 
-		pass
-	try:
-		score += PlayerStat.objects.get( player=pick.WR2, week=currentWeek ).score
-	except ObjectDoesNotExist: 
-		pass
-	try:
-		score += PlayerStat.objects.get( player=pick.PK, week=currentWeek ).score
-	except ObjectDoesNotExist: 
-		pass
-	try:
-		score += DefenseStat.objects.get( team=pick.TD, week=currentWeek ).score
-	except ObjectDoesNotExist:
-		pass
+for week in xrange( 0, Itthicron.currentWeek() + 1 ):
+   for pick in Picks.objects.filter( week=week ):
+           score = 0
+           try:
+                   score += PlayerStat.objects.get( player=pick.QB1, week=currentWeek ).score
+           except ObjectDoesNotExist: 
+                   pass
+           try:
+                   score += PlayerStat.objects.get( player=pick.QB2, week=currentWeek ).score
+           except ObjectDoesNotExist: 
+                   pass
+           try:
+                   score += PlayerStat.objects.get( player=pick.RB1, week=currentWeek ).score
+           except ObjectDoesNotExist: 
+                   pass
+           try:
+                   score += PlayerStat.objects.get( player=pick.RB2, week=currentWeek ).score
+           except ObjectDoesNotExist: 
+                   pass
+           try:
+                   score += PlayerStat.objects.get( player=pick.WR1, week=currentWeek ).score
+           except ObjectDoesNotExist: 
+                   pass
+           try:
+                   score += PlayerStat.objects.get( player=pick.WR2, week=currentWeek ).score
+           except ObjectDoesNotExist: 
+                   pass
+           try:
+                   score += PlayerStat.objects.get( player=pick.PK, week=currentWeek ).score
+           except ObjectDoesNotExist: 
+                   pass
+           try:
+                   score += DefenseStat.objects.get( team=pick.TD, week=currentWeek ).score
+           except ObjectDoesNotExist:
+                   pass
 
-	pick.score = score
-	pick.save()
+           pick.score = score
+           pick.save()
