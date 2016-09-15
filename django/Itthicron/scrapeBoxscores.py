70e9fc50950d232eeaed4e9cc10297af0480c05b
Reid Mayfield <rmayf@arista.com>
2016-09-15 01:06:59 -0700

    add output to scrapeBoxscores

--- django/Itthicron/scrapeBoxscores.py
+++ django/Itthicron/scrapeBoxscores.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import Itthicron
 import subprocess
+import os
 from stats.models import Game
+from sys import stderr
 
 os.chdir( "../" )
 sqlClearStatsInput = open( 'sql_clear_boxscore_stats.txt' )
@@ -10,7 +12,12 @@ subprocess.call( sqlClearStatsCmd.split(), stdin=sqlClearStatsInput )
 os.chdir( "../cfbScraper" )
 scrapyCmd = "scrapy crawl boxscore -a gameId=%s --logfile=scrapy_boxscore_crawl_game_%s.log"
 games = Game.objects.all()
+columns = "{0:^20}|{1:^20}|{2:^20}|{3:^20}"
+seperator = "{:-^80}"
+stderr.write( columns.format( "WEEK", "TEAM", "OPP", "ESPN_ID" ) + "\n" )
+stderr.write( seperator.format( "" ) + "\n" )
 for game in games:
 	if game.gameId is not None:
+                stderr.write( columns.format( game.week, game.team.name,
+                                              game.opponent.name, game.gameId ) + "\n" )
 		subprocess.call( ( scrapyCmd % ( game.gameId, game.gameId ) ).split() )
-		
